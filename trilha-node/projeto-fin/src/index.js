e25086d45bc6a0aee0e0624eada41efb4fe84eb5
isaacbrds <isaac.brigido@gmail.com>
2022-09-03 16:51:12 -0300

    Curso Javascript - Configurando Projeto Financeiro - Criando função para saque

--- trilha-node/projeto-fin/src/index.js
+++ trilha-node/projeto-fin/src/index.js
@@ -20,6 +20,18 @@ function verifyIfExistsAccountCPF(req, res, next) {
   return next();
 }
 
+function getBalance(statement) {
+  const balance = statement.reduce((acc, operation)=>{
+    if (operation.type === 'Credit'){
+      return acc + operation.amount;
+    }else{
+      return acc - operation.amount
+    }
+  }, 0);
+
+  return balance;
+}
+
 app.post('/v1/api/account', (req, res) => {
   const { cpf, name } = req.body;
   
@@ -62,6 +74,28 @@ app.post('/v1/api/deposit', verifyIfExistsAccountCPF, (req, res) => {
   customer.statement.push(statementOperation)
 
   return res.status(201).json( customer )
+})
+
+app.post('/v1/api/withdraw', verifyIfExistsAccountCPF, (req, res)=>{
+  const { amount } = req.body;
+
+  const { customer } = req;
+
+  const balance = getBalance(customer.statement);
+
+  if (balance < amount){
+    return res.status(400).json({error: "You don't have enough funds!"})
+  }
+
+  const statementOperation = {
+    amount,
+    created_at: new Date(),
+    type: 'Debit'
+  };
+
+  customer.statement.push(statementOperation);
+
+  return res.status(201).send()
 })
 
 app.listen(8000)
