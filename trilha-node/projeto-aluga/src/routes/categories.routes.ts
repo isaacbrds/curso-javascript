df1d9d69c3b17e6f97381161830832a89da7785a
isaacbrds <isaac.brigido@gmail.com>
2022-09-06 11:55:18 -0300

    Curso Javascript - Criando Repositorio para categoria

--- trilha-node/projeto-aluga/src/routes/categories.routes.ts
+++ trilha-node/projeto-aluga/src/routes/categories.routes.ts
@@ -1,26 +1,16 @@
 import { Router } from 'express';
-import  {v4 as uuidV4 } from 'uuid';
 import { Request, Response } from 'express';
-import {Category} from '../model/Category';
+import {CategoriesRepository} from '../repositories/CategoriesRepository'
 
 const categoriesRoutes = Router();
-
-const categories: Category[] = [];
+const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post("/", (req: Request, res: Response) => {
   const {name, description} = req.body;
 
-  const category = new Category();
+  categoriesRepository.create({name, description});
   
-  Object.assign(category, {
-    name, 
-    description,
-    created_at: new Date(),
-  });
-  
-  categories.push(category);
-
-  return res.status(201).json(category);
+  return res.status(201).send();
 })
 
 export {categoriesRoutes}; 
