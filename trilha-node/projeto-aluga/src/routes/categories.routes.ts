0972fad01bd1092ee95ee3ac8d6b55a37f549507
isaacbrds <isaac.brigido@gmail.com>
2022-09-08 10:19:21 -0300

    Curso Javascript - Criando Controller para Categoria

--- trilha-node/projeto-aluga/src/routes/categories.routes.ts
+++ trilha-node/projeto-aluga/src/routes/categories.routes.ts
@@ -1,25 +1,15 @@
 import { Router } from 'express';
-import { Request, Response } from 'express';
-import { CategoriesRepository } from '../modules/cars/repositories/CategoriesRepository'
-import { CreateCategoryService } from '../modules/cars/services/CreateCategoryService';
+import { createCategoryController } from '../modules/cars/useCases/createCategory';
+import { listCategoriesController} from '../modules/cars/useCases/listCategories';
 
 const categoriesRoutes = Router();
-const categoriesRepository = new CategoriesRepository();
 
-categoriesRoutes.post("/", (req: Request, res: Response) => {
-  const {name, description} = req.body;
-
-  const createCategoryService = new CreateCategoryService(categoriesRepository);
-
-  createCategoryService.execute({ name, description });
-  
-  return res.status(201).send();
+categoriesRoutes.post("/", (req, res) => {
+ return createCategoryController.handle(req, res);
 })
 
-categoriesRoutes.get('/', (req: Request, res: Response) => {
-  const all = categoriesRepository.list();
-
-  return res.status(200).json(all)
+categoriesRoutes.get('/', (req, res) => {
+ return listCategoriesController.handle(req, res);
 })
 
 export {categoriesRoutes}; 
