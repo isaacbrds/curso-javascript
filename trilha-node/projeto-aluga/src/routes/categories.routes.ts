8f69e630c88d5cafe4ad68a55c9ea35e87d11e8a
isaacbrds <isaac.brigido@gmail.com>
2022-09-09 10:26:11 -0300

    Curso Javascript - Criando Controller de especificações

--- trilha-node/projeto-aluga/src/routes/categories.routes.ts
+++ trilha-node/projeto-aluga/src/routes/categories.routes.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
-import { createCategoryController } from '../modules/cars/useCases/createCategory';
-import { listCategoriesController} from '../modules/cars/useCases/listCategories';
+import { createCategoryController } from '../modules/cars/useCases/createCategory/';
+import { listCategoriesController} from '../modules/cars/useCases/listCategories/';
 
 const categoriesRoutes = Router();
 
