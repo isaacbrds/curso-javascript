a635095502684b23ade5a94d89c9d0f3abc86106
isaacbrds <isaac.brigido@gmail.com>
2022-09-15 10:40:07 -0300

    Curso Javascript - Configurando import das categorias

--- trilha-node/projeto-aluga/src/routes/categories.routes.ts
+++ trilha-node/projeto-aluga/src/routes/categories.routes.ts
@@ -1,8 +1,13 @@
 import { Router } from 'express';
+import multer from 'multer';
 import { createCategoryController } from '../modules/cars/useCases/createCategory/';
 import { listCategoriesController} from '../modules/cars/useCases/listCategories/';
 
 const categoriesRoutes = Router();
+
+const upload = multer({
+  dest: "./tmp"
+});
 
 categoriesRoutes.post("/", (req, res) => {
  return createCategoryController.handle(req, res);
@@ -12,4 +17,9 @@ categoriesRoutes.get('/', (req, res) => {
  return listCategoriesController.handle(req, res);
 })
 
+categoriesRoutes.post('/import', upload.single("file"),(req, res) =>{
+  const {file} = req;
+  console.log(file);
+  return res.send()
+})
 export {categoriesRoutes}; 
